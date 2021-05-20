a2f05bd93dba69e1b4a3fa2081c200d1138d8c79
wiktorele <wiktor.ucinski@gmail.com>
2021-05-21 00:39:43 +0200

    Database connection + display in browser

---
+++ POS/Data/PosModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POS.Data
+{
+    public class PosModel
+    {
+        public DateTime Date { get; set; }
+        public int Id { get; set; }
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+        public int Co2 { get; set; }
+    }
+}

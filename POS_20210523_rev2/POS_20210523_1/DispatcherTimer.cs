b1a7ff53babc972c28de9f3474236f6bf4834418
StephenFox96 <83642277+StephenFox96@users.noreply.github.com>
2021-05-26 11:16:36 +0200

    Add files via upload

---
+++ POS_20210523_rev2/POS_20210523_1/DispatcherTimer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POS_20210523_1
+{
+    class DispatcherTimer
+    {
+        /// Occurs when the timer interval has elapsed.
+        internal static EventHandler Tick;
+
+        /// Gets or sets the period of time between timer ticks.
+        public static TimeSpan Interval { get; internal set; }
+
+        /// Method that starts the DispatcherTimer.
+        public static void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
